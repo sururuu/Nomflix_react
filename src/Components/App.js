90f9a65c633c629275805b61a4237477aa5be749
sujin <tkdgus700@naver.com>
2020-09-03 15:15:57 +0900

    CSS in React part One

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Router from "Components/Router";
 import Header from "Components/Header";
-
+// 위줄의 형태는 기본적으로 해당 폴더로 가서 index 파일을 보게 해줌.
+// Header 안에 index.js 를 굳이 만드는 이유는 Components/Header/Header 로 쓰지 않기 위해. 
 function App() {
   return (
     <>
