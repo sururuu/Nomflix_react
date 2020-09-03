05e23aa99821bbb4a4c158a0008db1873beea112
sujin <tkdgus700@naver.com>
2020-09-03 11:07:43 +0900

    React Router part Two

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Router from "Components/Router";
+import Header from "Components/Header";
+
 function App() {
   return (
-    <Router />
+    <>
+      <Header />
+      <Router />
+    </>
   );
 }
 
