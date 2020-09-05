713618ff34325170e69e459ead02a4e3c9b62dbf
sujin <tkdgus700@naver.com>
2020-09-05 18:13:38 +0900

    HomeContainer

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,17 +1,46 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { moviesApi } from "api";
 
 export default class extends React.Component {
   state = {
     nowPlaying: null,
-    upcoming:null,
+    upcoming: null,
     popular: null,
     error: null,
     loading: true
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying }
+      } = await moviesApi.nowPlaying();
+      const {
+        data: { results: upcoming }
+      } = await moviesApi.upcoming();
+      const {
+        data: { results: popular }
+      } = await moviesApi.popular();
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular
+      });
+    } catch {
+      this.setState({
+        error: "Can't find movies information."
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  }
+
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
+    alter('되냐?')
     return (
       <HomePresenter
         nowPlaying={nowPlaying}
@@ -19,7 +48,7 @@ export default class extends React.Component {
         popular={popular}
         error={error}
         loading={loading}
-         />
-    )
+      />
+    );
   }
 }
